d4de9aacd22d396df2033008601d15b5463d82f7
Morgan_Muller <morgaine.muller@gmail-com>
2024-08-05 00:00:24 +0200

    first commit

---
+++ js/inhoudFlirtertwister.js
@@ -0,0 +1,157 @@
+
+//data arrays -> !!HIER de scenario's aanpassen: haakjes en aanhalingstekens moeten blijven staan!!
+
+//Volwassenen
+
+let volwassenenWie = [
+    {
+      text: "Je baas",
+      color: "#9CCFB0",
+    },
+    { 
+      text: "Je vriend",
+      color: "#B5DFD1",
+    },
+    { 
+      text: "Je date",
+      color: "#9CCFB0",
+    },
+    {
+      text: "Je sekspartner",
+      color: "#B5DFD1",
+    },
+    {
+      text: "Je verloofde",
+      color: "#9CCFB0",
+    },
+    {
+      text: "Je tinder match",
+      color: "#B5DFD1",
+    },
+    {
+      text: "Een wildvreemde",
+      color: "#9CCFB0",
+    },
+    {
+      text: "Een buur",
+      color: "#B5DFD1",
+    },
+    {
+      text: "Je ex",
+      color: "#9CCFB0",
+    },
+    {
+      text: "Je collega",
+      color: "#B5DFD1",
+    },
+    {
+      text: "Je partner",
+      color: "#9CCFB0",
+    },
+    {
+      text: "Je bestie",
+      color: "#B5DFD1",
+    }
+  ];
+
+let volwassenenWat = [
+    {
+      text: "kust je",
+      color: "#F4C7C4",
+    },
+    { 
+      text: "raakt je gat aan",
+      color: "#F3CDB0",
+    },
+    { 
+      text: "ruikt aan je haar",
+      color: "#F4C7C4",
+    },
+    {
+      text: "vraagt je nummer",
+      color: "#F3CDB0",
+    },
+    {
+      text: "praat over wilde seks",
+      color: "#F4C7C4",
+    },
+    {
+      text: "geeft je een knuffel",
+      color: "#F3CDB0",
+    },
+    {
+      text: "stuurt je een naaktfoto",
+      color: "#F4C7C4",
+    },
+    {
+      text: "flirt met jou",
+      color: "#F3CDB0",
+    },
+    {
+      text: "geeft je een knipoog",
+      color: "#F4C7C4",
+    },
+    {
+      text: "noemt je sexy",
+      color: "#F3CDB0",
+    },
+    {
+      text: "geeft je een cadeau",
+      color: "#F4C7C4",
+    },
+    {
+      text: "vraagt je uit",
+      color: "#F3CDB0",
+    }
+  ];
+
+  let volwassenenWaar = [
+    {
+      text: "op je werk",
+      color: "#A6B2D8",
+    },
+    { 
+      text: "in de supermarkt",
+      color: "#A894C5",
+    },
+    { 
+      text: "op een feestje",
+      color: "#A6B2D8",
+    },
+    {
+      text: "bij een etentje",
+      color: "#A894C5",
+    },
+    {
+      text: "in een park",
+      color: "#A6B2D8",
+    },
+    {
+      text: "in je woonkamer",
+      color: "#A894C5",
+    },
+    {
+      text: "op vakantie",
+      color: "#A6B2D8",
+    },
+    {
+      text: "in het zwembad",
+      color: "#A894C5",
+    },
+    {
+      text: "op sollicitatiegesprek",
+      color: "#A6B2D8",
+    },
+    {
+      text: "in de auto",
+      color: "#A894C5",
+    },
+    {
+      text: "op school",
+      color: "#A6B2D8",
+    },
+    {
+      text: "op toilet",
+      color: "#A894C5",
+    }
+  ];
---
+++ js/script.js
@@ -0,0 +1,168 @@
+
+
+let wheel = document.querySelector(".wheel");
+let spinner = wheel.querySelector(".spinner");
+let volgende = wheel.querySelector(".btn-verder");
+const trigger = wheel.querySelector(".btn-spin");
+const ticker = wheel.querySelector(".ticker");
+let selectedDisplay = document.querySelector("#displaySelected");
+
+const spinClass = "is-spinning";
+const selectedClass = "selected";
+const spinnerStyles = window.getComputedStyle(spinner);
+let tickerAnim;
+let rotation = 0;
+let currentSlice = 0;
+let prizeNodes;
+// is het Wie wat of waar? Belangrijk want het bepaalt de waarden daarom hebben we een variabele die dit bijhoudt 1 is wie, 2 is wat en 3 is waar
+let WieWatOfWaar = 1;
+let gekozenWieWaarde=document.querySelector("#displayWie"); 
+let gekozenWatWaarde =document.querySelector("#displayWat");
+let gekozenWaarWaarde =document.querySelector("#displayWaar");
+
+console.log(WieWatOfWaar);
+let valuesWheel = volwassenenWie;
+
+const prizeSlice = 360 / valuesWheel.length;
+const prizeOffset = Math.floor(180 / valuesWheel.length);
+
+const changeValues = () => {
+if(WieWatOfWaar===1){
+  valuesWheel = volwassenenWie;
+  selectedDisplay.innerHTML= "Wie?"
+  gekozenWieWaarde.classList.add("vet");
+ }else if(WieWatOfWaar===2){
+  valuesWheel = volwassenenWat;
+  selectedDisplay.innerHTML= "Wat?"
+  gekozenWieWaarde.classList.remove("vet");
+  gekozenWieWaarde.classList.add("wasVet");
+  gekozenWatWaarde.classList.add("vet");
+ }else if(WieWatOfWaar===3){
+  valuesWheel = volwassenenWaar;
+  selectedDisplay.innerHTML= "Waar?"
+  gekozenWatWaarde.classList.remove("vet");
+  gekozenWatWaarde.classList.add("wasVet");
+  gekozenWaarWaarde.classList.add("vet");
+ }
+setupWheel()
+};
+
+
+
+
+const createPrizeNodes = () => {
+  valuesWheel.forEach(({ text, color}, i) => {
+    const rotation = ((prizeSlice * i) * -1) - prizeOffset;
+    
+    spinner.insertAdjacentHTML(
+      "beforeend",
+      `<li class="prize" style="--rotate: ${rotation}deg">
+        <span class="text">${text}</span>
+      </li>`
+    );
+  });
+};
+
+const createConicGradient = () => {
+  spinner.setAttribute(
+    "style",
+    `background: conic-gradient(
+      from -90deg,
+      ${valuesWheel
+        .map(({ color }, i) => `${color} 0 ${(100 / valuesWheel.length) * (valuesWheel.length - i)}%`)
+        .reverse()
+      }
+    );`
+  );
+};
+
+
+const setupWheel = () => {
+  createConicGradient();
+  createPrizeNodes();
+  prizeNodes = wheel.querySelectorAll(".prize");
+};
+
+const spinertia = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const runTickerAnimation = () => {
+  // https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+  const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];  
+  let rad = Math.atan2(b, a);
+  
+  if (rad < 0) rad += (2 * Math.PI);
+  
+  const angle = Math.round(rad * (180 / Math.PI));
+  const slice = Math.floor(angle / prizeSlice);
+
+  if (currentSlice !== slice) {
+    ticker.style.animation = "none";
+    setTimeout(() => ticker.style.animation = null, 10);
+    currentSlice = slice;
+  }
+
+  tickerAnim = requestAnimationFrame(runTickerAnimation);
+};
+
+
+
+const selectPrize = () => {
+  const selected = Math.floor(rotation / prizeSlice);
+  prizeNodes[selected].classList.add(selectedClass);
+  selectedDisplay.innerHTML= prizeNodes[selected].innerHTML;
+  volgende.classList.remove("invisible");
+  if(WieWatOfWaar===1){
+    gekozenWieWaarde.innerHTML= prizeNodes[selected].innerHTML;
+   } else if(WieWatOfWaar===2){
+    gekozenWatWaarde.innerHTML= prizeNodes[selected].innerHTML;
+   } else if(WieWatOfWaar===3){
+    gekozenWaarWaarde.innerHTML= prizeNodes[selected].innerHTML.trim() +".";
+    volgende.innerHTML = "Opnieuw spelen?";
+    gekozenWieWaarde.classList.add("vet");
+    gekozenWatWaarde.classList.add("vet");
+   }
+   trigger.classList.add("invisible");
+};
+
+trigger.addEventListener("click", () => {
+  trigger.disabled = true;
+  rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
+  prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+  wheel.classList.add(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  ticker.style.animation = "none";
+  runTickerAnimation();
+});
+
+spinner.addEventListener("transitionend", () => {
+  cancelAnimationFrame(tickerAnim);
+  trigger.disabled = false;
+ // trigger.focus();
+  rotation %= 360;
+  selectPrize();
+  wheel.classList.remove(spinClass);
+  spinner.classList.add("opacity");
+  spinner.style.setProperty("--rotate", rotation);
+});
+
+volgende.addEventListener("click", () => {
+  spinner.classList.remove("opacity");
+  wheel.querySelectorAll(".prize").forEach(e=> e.remove());
+  if(WieWatOfWaar<3){
+    WieWatOfWaar++;
+    console.log(WieWatOfWaar);
+    changeValues();
+   } else if(WieWatOfWaar===3){
+    location.reload();
+   }
+   volgende.classList.add("invisible");
+   trigger.classList.remove("invisible");
+});
+
+setupWheel();
